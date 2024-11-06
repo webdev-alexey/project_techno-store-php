cd525093867fdcdadae0e4360cd92a03bdbfc2c5
Alexey <alexey.anasov@yandex.by>
2024-11-06 23:37:45 +0300

    разбивание на шаблоны в php

---
+++ templates/_footer.php
@@ -0,0 +1,11 @@
+<div class="copyright">
+		<p>
+			Сделал данный проект в рамках интенсива от
+			<a href="http://webcademy.ru" target="_blank">WebCademy.ru</a>
+		</p>
+	</div>
+
+	<script src="js/main.js"></script>
+</body>
+
+</html>
